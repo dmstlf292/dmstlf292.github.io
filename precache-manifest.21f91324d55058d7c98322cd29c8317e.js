7b828013a2f39ec3ca5faea7a7cc37fecddbd475
dmstlf292 <silviapark292@gmail.com>
2021-04-08 00:04:56 +0900

    Updates

---
+++ precache-manifest.21f91324d55058d7c98322cd29c8317e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a844b1d8d2706b2a0fa3740cb1cf145d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0060fdc10130c5592324",
+    "url": "/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0060fdc10130c5592324",
+    "url": "/static/js/main.23d2406c.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "e0dc34436c6d13b9d99d578e2e1c1626",
+    "url": "/static/media/your photo.e0dc3443.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d77e72149e7c59f5fc7bb786c253d8c8.js"
+  "/precache-manifest.21f91324d55058d7c98322cd29c8317e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
