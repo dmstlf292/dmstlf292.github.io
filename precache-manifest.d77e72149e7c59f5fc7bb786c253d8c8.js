b273c8d30468f451c3ce0aec8a5c4f607c3b691a
dmstlf292 <silviapark292@gmail.com>
2021-04-07 23:53:34 +0900

    Updates

---
+++ precache-manifest.d77e72149e7c59f5fc7bb786c253d8c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f72a07b3a79e41016a79d6c164176bf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0060fdc10130c5592324",
+    "url": "/portfolio/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/portfolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0060fdc10130c5592324",
+    "url": "/portfolio/static/js/main.23d2406c.chunk.js"
+  },
+  {
+    "revision": "26351f6e611646d04f58",
+    "url": "/portfolio/static/js/runtime-main.94e8fcfa.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/portfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "e0dc34436c6d13b9d99d578e2e1c1626",
+    "url": "/portfolio/static/media/your photo.e0dc3443.png"
+  }
+]);
